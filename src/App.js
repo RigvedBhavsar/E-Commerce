a8265530e1db36c2a3c60e3db3d778a7d3a476b2
RigvedBhavsar <rigvedbhavsar@gmail.com>
2021-06-08 08:48:50 +0530

    added funcnality of cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React,{useState , useEffect} from 'react';
 import { commerce } from './lib/commerce';
-import {Products , Navbar} from './componets';
+import {Products , Navbar , Cart} from './componets';
 
 const App = () => {
     const [products , setProducts] = useState([]);
+    const [cart , setCart] = useState({});
 
     const fetchProducts = async ()=> { 
         const {data} = await commerce.products.list();
         setProducts(data);
     }
 
+    const fetchCart = async () =>{
+        const cart = await commerce.cart.retrieve();
+        setCart(cart);
+    }
+
+    const handleAddToCart = async(productId , quantity ) =>{
+        const item = await commerce.cart.add(productId , quantity);
+        setCart(item.cart);
+    }
+
+    console.log(cart);
+    // console.log(cart.line_items.length);
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     },[]);
 
     
 
     return (
         <div>
-            <Navbar/>
-            <Products products= {products}/>
+            <Navbar totalItems ={cart.total_items}/>
+            {/* <Products products={products}  onAddToCart={handleAddToCart}/> */}
+            <Cart cart={cart}/>
         </div>
     )
 }
--- src/componets/Products/Product/Product.jsx
+++ src/componets/Products/Product/Product.jsx
@@ -5,7 +5,7 @@ import useStyles from './styles';
 
 
 
-const Product = ({product}) => {
+const Product = ({product , onAddToCart}) => {
 
     const classes = useStyles();
     
@@ -24,7 +24,7 @@ const Product = ({product}) => {
                 <Typography dangerouslySetInnerHTML={{ __html : product.description }}/>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id , 1)}>
                     <AddShoppingCart/>
                 </IconButton>
             </CardActions>
